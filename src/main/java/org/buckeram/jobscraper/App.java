4b3e4343418dcda9ad755fb63d92cf986e183e0b
Alan Buckeridge <alanbuckeridge@gmail.com>
2016-06-11 18:43:26 +0100

    Initial commit

---
+++ src/main/java/org/buckeram/jobscraper/App.java
@@ -0,0 +1,13 @@
+package org.buckeram.jobscraper;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
